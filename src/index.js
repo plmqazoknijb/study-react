cc681f80c79895e3977d560fde7a56f01d2764fd
plmqazoknijb <plmqazoknijb@naver.com>
2022-03-31 14:11:43 +0900

    220331 수업실습

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@
 //import './component-basic/first-component'
 //import './component-basic/props-basic'
 //import './component-basic/props-children'
-import './component-intermediate/state-study-1'
+//import './component-intermediate/state-study-1'
+import './component-intermediate/manipulate-list'
